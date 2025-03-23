f27f50bab8f020e56e11c853aac14f68d4911b37
RedHBlack <billyvilleroy@gmail.com>
2025-03-23 04:34:24 +0100

    Basic Block logic

--- compiler/IR/BasicBlock.cpp
+++ compiler/IR/BasicBlock.cpp
@@ -1,13 +1,23 @@
 #include "BasicBlock.h"
 
-BasicBlock::BasicBlock(CFG *cfg, string entry_label)
+BasicBlock::BasicBlock(CFG *cfg, string entry_label) : cfg(cfg), label(entry_label)
 {
 }
 
 void BasicBlock::gen_asm(ostream &o)
 {
+    for (int i = 0; i < instrs.size(); i++)
+    {
+        instrs[i]->gen_asm(o);
+    }
 }
 
-void BasicBlock::add_IRInstr(Operation op, Type t, vector<string> params)
+void BasicBlock::add_IRInstr(BaseIRInstr *instr)
 {
+    instrs.push_back(instr);
+}
+
+CFG *BasicBlock::getCFG()
+{
+    return this->cfg;
 }
